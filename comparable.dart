b13ba78479f1a2f6465d66eb17563ebf68e378d0
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-11-03 18:12:08 +0800

    Update and rename splay_tree_set_error.dart to comparable.dart

--- comparable.dart
+++ comparable.dart
@@ -1,10 +1,22 @@
-import 'dart:collection';
+//class Category{
+ // String id;
+ // String name;
+  
+ // Category(this.id, this.name);
+//}
+//implement comparable
 
-class Category{
+import 'dart:collection';
+class Category implements Comparable<Category>{
   String id;
   String name;
   
   Category(this.id, this.name);
+  
+  @override
+  int compareTo(Category other){
+    return id.compareTo(other.id);
+  }
 }
 
 void main(){
