8a09fea2aa9c8c105de717525f002edb3f874aa4
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-11-03 22:37:22 +0800

    Create hash_map.dart

---
+++ hash_map.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+void main(){
+  final scores = HashMap<String, int>();
+
+  scores['Bibah'] = 100;
+  scores['Dini'] = 100;
+  scores['Zahra'] = 100;
+  scores['Wulan'] = 100;
+  scores['Asa'] = 100;
+
+  print(scores);
+}
