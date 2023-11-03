d04628e25c286d5597c4ebb3edf8efea3007dd7a
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-11-03 18:14:49 +0800

    Create comparator.dart

---
+++ comparator.dart
@@ -0,0 +1,9 @@
+import 'dart:collection';
+
+void main(){
+  
+  final treeSet = SplayTreeSet<int>((first, second) => second.compareTo(first));
+  treeSet.addAll([1, 6, 5, 4, 3, 2, 7, 8, 9]);
+  
+  print(treeSet);
+}
